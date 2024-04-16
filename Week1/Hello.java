b03b17e1c8844f76ff561a50771944e3feb844c2
Prabin17np <prabin17np@gmail.com>
2024-04-16 23:16:24 +0545

    changes

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -2,6 +2,94 @@
 public class Hello{
 
     public static void main(String[] args){
-     System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    /* 2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
+    System.out.print(" This is my first program \n I am on module ST4003CEM");
+
+   /*  3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as "*/
+    System.out.println("A \"quoted\"string is");
+    System.out.println("\'much\' better if you learn");
+    System.out.println("the rules of \"escape sequences. \"\n");
+    System.out.println("Also, \"\" represents an empty\n");
+    System.out.println("String . Don't forgot: use \\\" \n");
+    System.out.println("instead of \" !\n");
+    System.out.println("'' is not the same as \"");
+
+   /*  4. WAP that prints the following pattern:
+
+*
+
+**
+
+***
+
+*****/
+
+System.out.println("* \n");
+System.out.println("** \n");
+System.out.println("*** \n");
+System.out.println("***** \n");
+/*5. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+********* */
+System.out.println("********* \n");
+System.out.println("********* \n");
+System.out.println("********* \n");
+System.out.println("********* \n");
+
+/*6. WAP that prints the following output:
+
+A well-formed Java program has a main method with { and } braces.
+
+
+
+A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+
+(But we type \" instead!)
+
+What is the difference between
+
+a ' and a "?  Or between a " and a \"?
+
+
+
+One is what we see when we're typing our program. The other is what appears on the "console." */
+
+System.out.println("A well-formed Java program has a main method with { and } braces.\n");
+System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character. \n");
+System.out.println("(But we type \\\" instead!)\n");
+System.out.println("What is the difference between\n");
+System.out.println("a ' and a \"?  Or between a \" and a \\\"?\n");
+System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
+
     }
+
 }
+    
+    
